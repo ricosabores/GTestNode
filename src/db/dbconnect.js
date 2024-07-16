3ad56f0591c610c02f1e5ecbac726133fc25424d
ricosabores <ricosabores@gmail.com>
2024-07-16 08:48:34 -0300

    Update dbconnect.js
    .

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-	host: 'sql10.freesqldatabase.com',
-	user: 'sql10717369',
-	password: 'rifDrgcYVh',
-	database: 'sql10717369',
+	host: 'mysql-ricosabores.alwaysdata.net',
+	user: '369367',
+	password: '@Ntonella1',
+	database: 'ricosabores_db',
 	port: 3306,
 	waitForConnections: true,
 	connectionLimit: 10,
